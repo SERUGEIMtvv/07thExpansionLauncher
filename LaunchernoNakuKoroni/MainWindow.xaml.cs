765b63613a01eb81deea6881a6845d25d0d5d93b
Sergey Matveev <stresha75@gmail.com>
2020-05-30 12:20:37 +0300

    Added AppLocale option.

--- LaunchernoNakuKoroni/MainWindow.xaml.cs
+++ LaunchernoNakuKoroni/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ using System.Threading;
 using System.Drawing.Text;
 using System.Runtime.InteropServices;
 using System.Resources;
-
+using System.Diagnostics;
 
 namespace LaunchernoNakuKoroni
 {
@@ -33,51 +33,68 @@ namespace LaunchernoNakuKoroni
             InitializeComponent();
             Title = Properties.Resources.Title;
         }
-        private void LaunchGame(string gameName)
+        private void LaunchGame(string gameName, string gameParams)
         {
             string path = ConfigurationManager.AppSettings[gameName];
+            string path2 = ConfigurationManager.AppSettings[gameParams];
+            ///check if the first string is empty and complain if it is
             if (path == null)
             {
                 MessageBox.Show(Properties.Resources.CantFindPath, Properties.Resources.CantFindPathTitle);
                 return;
             }
-
+            ///check if the path in the first string is correct and complain if it isn't
             if (!System.IO.File.Exists(path))
             {
                 MessageBox.Show(Properties.Resources.CantFindGame, Properties.Resources.CantFindGameTitle);
                 return;
             }
-
             try
             {
-                System.Diagnostics.Process.Start(path);
+                ///start the Locale Emulator and the requested game as its parameter
+                Process.Start(path, path2);
+                ///if the second parameter is empty, consider the first string as the game itself
+                ///and start it without any parameters
+                if (path2 == null)
+                {
+                    Process.Start(path);
+                }
+                ///check if the path in the second string is correct and complain if it isn't
+                if (path2 != null && !System.IO.File.Exists(path2))
+                {
+                    MessageBox.Show(Properties.Resources.CantFindGame, Properties.Resources.CantFindGameTitle);
+                    return;
+                }
             }
+            ///complain at anything weird
             catch (Exception)
             {
                 MessageBox.Show(Properties.Resources.UnknownError, Properties.Resources.UnknownErrorTitle);
             }
         }
-
-        private void ButtonHigQ1_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi1");
-        private void ButtonHigQ2_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi2");
-        private void ButtonHigQ3_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi3");
-        private void ButtonHigQ4_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi4");
-        private void ButtonHigA1_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi5");
-        private void ButtonHigA2_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi6");
-        private void ButtonHigA3_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi7");
-        private void ButtonHigA4_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi8");
-        private void ButtonHigRei_Click(object sender, RoutedEventArgs e) => LaunchGame("HigurashiRei");
-        private void ButtonHigHou_Click(object sender, RoutedEventArgs e) => LaunchGame("HigurashiHou");
-        private void ButtonHigCon_Click(object sender, RoutedEventArgs e) => LaunchGame("HigurashiConsole");
-        private void ButtonUmiQ_Click(object sender, RoutedEventArgs e) => LaunchGame("UminekoQuestion");
-        private void ButtonUmiC_Click(object sender, RoutedEventArgs e) => LaunchGame("UminekoSaku");
-        private void ButtonUmiT_Click(object sender, RoutedEventArgs e) => LaunchGame("UminakoTsubasa");
-        private void ButtonUmiH_Click(object sender, RoutedEventArgs e) => LaunchGame("UminekoHane");
-        private void ButtonUmiS_Click(object sender, RoutedEventArgs e) => LaunchGame("UminekoSaku");
-        private void ButtonUmi07T_Click(object sender, RoutedEventArgs e) => LaunchGame("07thTheatre");
-        private void ButtonCic1_Click(object sender, RoutedEventArgs e) => LaunchGame("Ciconia1");
-        private void ButtonCic2_Click(object sender, RoutedEventArgs e) => LaunchGame("Ciconia2");
-        private void ButtonCic3_Click(object sender, RoutedEventArgs e) => LaunchGame("Ciconia3");
-        private void ButtonCic4_Click(object sender, RoutedEventArgs e) => LaunchGame("Ciconia4");
+        /// <summary>
+        /// Actions for the buttons
+        /// </summary>
+        private void ButtonHigQ1_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi1", "Higurashi1Param");
+        private void ButtonHigQ2_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi2", "Higurashi2Param");
+        private void ButtonHigQ3_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi3", "Higurashi3Param");
+        private void ButtonHigQ4_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi4", "Higurashi4Param");
+        private void ButtonHigA1_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi5", "Higurashi5Param");
+        private void ButtonHigA2_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi6", "Higurashi6Param");
+        private void ButtonHigA3_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi7", "Higurashi7Param");
+        private void ButtonHigA4_Click(object sender, RoutedEventArgs e) => LaunchGame("Higurashi8", "Higurashi8Param");
+        private void ButtonHigRei_Click(object sender, RoutedEventArgs e) => LaunchGame("HigurashiRei", "HigurashiReiParam");
+        private void ButtonHigHou_Click(object sender, RoutedEventArgs e) => LaunchGame("HigurashiHou", "HigurashiHouParam");
+        private void ButtonHigCon_Click(object sender, RoutedEventArgs e) => LaunchGame("HigurashiConsole", "HigurashiConsoleParam");
+        private void ButtonUmiQ_Click(object sender, RoutedEventArgs e) => LaunchGame("UminekoQuestion", "UminekoQuestionParam");
+        private void ButtonUmiC_Click(object sender, RoutedEventArgs e) => LaunchGame("UminekoSaku", "UminekoSakuParam");
+        private void ButtonUmiT_Click(object sender, RoutedEventArgs e) => LaunchGame("UminakoTsubasa", "UminakoTsubasaParam");
+        private void ButtonUmiH_Click(object sender, RoutedEventArgs e) => LaunchGame("UminekoHane", "UminekoHaneParam");
+        private void ButtonUmiS_Click(object sender, RoutedEventArgs e) => LaunchGame("UminekoSaku", "UminekoSakuParam");
+        private void ButtonUmi07T_Click(object sender, RoutedEventArgs e) => LaunchGame("07thTheatre", "07thTheatreParam");
+        private void ButtonCic1_Click(object sender, RoutedEventArgs e) => LaunchGame("Ciconia1", "Ciconia1Param");
+        private void ButtonCic2_Click(object sender, RoutedEventArgs e) => LaunchGame("Ciconia2", "Ciconia2Param");
+        private void ButtonCic3_Click(object sender, RoutedEventArgs e) => LaunchGame("Ciconia3", "Ciconia3Param");
+        private void ButtonCic4_Click(object sender, RoutedEventArgs e) => LaunchGame("Ciconia4", "Ciconia4Param");
     }
 }
